f92383c1cc1c849679ffa3182bf4ccb6cb61d8e7
DominicKafka <dominic.kafka@gmail.com>
2014-03-24 17:20:55 +0200

    Separation of the input section from the main code for converted mesher

--- quad8_mesh.py
+++ quad8_mesh.py
@@ -11,34 +11,9 @@ Created on Sun Mar 23 12:32:57 2014
 
 import math
 
-h = float(input('Height of rectangle ? '))
-l = float(input('Length of rectangle ? '))
-m = int(input('Increments along height ? '))
-n = int(input('Increments along length ? '))
-option = int(input('0:Plane strain or 1:Plane stress ? '))
-E = int(input('Elasticity modulus ? '))
-nu = float(input('Poisson\'s ratio ? '))
-t = float(input('Element thickness ? '))
-print('0: Parabolic shear stress or')
-print('1: Linear bending stress at beam tip or')
-print('2: Beam forced into radius or')
-print('3: Beam transformed to 90 degree section loaded with pressure')
-load_opt = int(input('4: Beam transformed to 90 degree section, inside surface displaced '))
+from mesh_inputs import mesh_inputs
 
-if load_opt == 0:
-    V0 = float(input('Magnitude of total shear force at beam tip ? '))
-elif load_opt == 1:
-    V0 = float(input('Magnitude of bending stress at top surface at beam tip ? '))
-elif load_opt == 2:
-    R = float(input('Enter radius that beam is deformed into. '))
-elif load_opt == 3:
-    R = 2. * l / math.pi
-    Mag = float(input('Internal pressure (in undeformed configuration) ? '))
-elif load_opt == 4:
-    R = 2 * l / math.pi
-    RadD = float(input('Radial displacement ? '))
-
-filen = raw_input('Write output to which file ? ')
+(h, l, m, n, option, E, nu, t, load_opt, V0, R, Mag, RadD, filen) = mesh_inputs()
 
 coord = []
 displ = []
