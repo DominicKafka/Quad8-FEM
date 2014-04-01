52d755188ddd8167b6a97b2e8d0c35fba1d9d494
Carl Sandrock <carl.sandrock@up.ac.za>
2014-04-01 05:11:40 +0200

    Added basic tests for Svec_to_Smat

--- test_Svec_to_Smat.m
+++ test_Svec_to_Smat.m
@@ -3,9 +3,7 @@ Svec = 1:3;
 
 Smat = Svec_to_Smat(Svec);
 
-% Smat must be symmetrical
 assert(all(all(Smat == Smat')), ...
        'Smat is not symmetrical');
-% Smat must be 4x4
 assert(all(size(Smat) == [4, 4]), ...
-       'Smat must be 4x4')
+       'Smat must be 4x4');
