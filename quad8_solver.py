8e0807d21b7a277a68fe41be37e22cc5477c69f0
Carl Sandrock <carl.sandrock@up.ac.za>
2014-04-03 10:25:05 +0200

    Fixed equality instead of assign

--- quad8_solver.py
+++ quad8_solver.py
@@ -16,7 +16,7 @@ from block_diag import block_diag
 ElType = 'q8'
 NodesPerEl = 0
 if ElType in ['q4', '5B', '7B']:
-    NodesPerEl == 4
+    NodesPerEl = 4
 elif ElType == 'q8':
     NodesPerEl = 8
 
