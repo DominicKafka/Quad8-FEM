74dffcfc5cba79c94840f95b6a884779ef6cb4c7
DominicKafka <dominic.kafka@gmail.com>
2014-04-28 18:19:40 +0200

    B_Quad8.py updated and tested, as well as test file

---
+++ test_B_Quad8.py
@@ -0,0 +1,38 @@
+# Basic test from random operation loop in Beam2by20
+
+import unittest
+import numpy as np
+from B_Quad8 import B_Quad8
+
+
+class B_Quad8TestCase(unittest.TestCase):
+    def testB_Quad8(self):
+
+        xi = 0.57735
+        eta = -0.57735
+        X = np.array([[8, -0.5], [8.5, -0.5], [8.5, 0.], [8, 0], [8.25, -0.5],
+        [8.5, -0.25], [8.25, 0.], [8, -0.25]])
+        true = 1
+
+        [B, detJ] = B_Quad8(xi, eta, X, true)
+
+        Bdash = np.matrix([[0.91068, 0.0000, 2.73205, 0.0000, 0.24402, 0.0000,
+         0.73205, 0.0000, -3.64273, 0.0000, 1.33333, 0.0000, -0.97607, 0.0000,
+          -1.33333, 0.0000],
+        [0.0000, -0.24402, 0.0000, -2.73205, 0.0000, -0.91068, 0.0000, -0.73205,
+         0.0000, -1.33333, 0.0000, 3.64273, 0.0000, 1.33333, 0.0000, 0.97607],
+        [-0.24402, 0.0000, -2.73205, 0.0000, -0.91068, 0.0000, -0.73205, 0.0000,
+         -1.33333, 0.0000, 3.64273, 0.0000, 1.33333, 0.0000, 0.97607, 0.0000],
+    [0.0000, 0.91068, 0.0000, 2.73205, 0.0000, 0.24402, 0.0000, 0.73205, 0.0000,
+         -3.64273, 0.0000, 1.33333, 0.0000, -0.97607, 0.0000, -1.33333]])
+
+        detJdash = 0.062500
+
+        np.testing.assert_almost_equal(B,
+             Bdash, decimal=3, err_msg='Incorrect B Matrix', verbose=True)
+
+        np.testing.assert_almost_equal(detJ,
+             detJdash, 4, "Incorrect determinant")
+
+if __name__ == '__main__':
+    unittest.main()
